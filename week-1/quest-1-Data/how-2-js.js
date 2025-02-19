27f64fc841426ca1784b320a85872f70612bfacc
amellagu <amellagu>
2025-02-19 10:33:41 +0100

    how 2 js

--- week-1/quest-1-Data/how-2-js.js
+++ week-1/quest-1-Data/how-2-js.js
@@ -1,1 +1,16 @@
+// Let's make a hello world. First we create the JavaScript file
+
+// $ echo "console.log('Hello World')" > how-2-js.js
+// To run JavaScript in your browser, you can import it into a HTML page:
+
+// $ echo '<script type="module" src="how-2-js.js"></script>' > index.html
+// Let's create a simple web server:
+
+// $ &>/dev/null python3 -m http.server &
+
+// get PID wait command [ ps ] = get all ports active in the device
+
+// kill any server whit [ kill <PID> ] = kill 8800
+
+
 console.log('Hello World')
