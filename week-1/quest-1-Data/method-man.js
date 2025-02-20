6cde43dee0fac3b6b29ac527817910be435dffd7
amellagu <amellagu>
2025-02-20 19:45:10 +0100

    add some changes

--- week-1/quest-1-Data/method-man.js
+++ week-1/quest-1-Data/method-man.js
@@ -1,8 +1,8 @@
 const words = (str) => str.split(" ");
 const sentence = (arr) => arr.join(" ");
 const yell = (str) => str.toUpperCase();
-const whisper = (str) => str.toLowerCase();
-const capitalize = (str) => yell(str[0]) + whisper(str.slice(1))
+const whisper = (str) => '*' + str.toLowerCase() + '*';
+const capitalize = (str) => yell(str[0]) + str.slice(1).toLowerCase();
 
 const str = 'lello world erAAAAAre eAAAAarer eer Are ere';
 
