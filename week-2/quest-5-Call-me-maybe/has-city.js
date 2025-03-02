7d2c0a6628b73f4e1e7e93329ef6f0d1ef299f66
amellagu <amellagu>
2025-03-02 12:04:41 +0000

    add some files

---
+++ week-2/quest-5-Call-me-maybe/has-city.js
@@ -0,0 +1,1 @@
+const hasCity = (country, arr) => (city) => arr.includes(city)? `${city} is a city from ${country}`: `${city} is not a city from ${country}`;
---
+++ week-2/quest-6-DOM/get-them-all/get-them-all.js
@@ -0,0 +1,7 @@
+export const getArchitects = () => [document.querySelectorAll('a'), document.querySelectorAll('span')]
+
+export const getClassical = () => [document.querySelectorAll('a.classical'), document.querySelectorAll('a:not(.classical)')]
+
+export const getActive = () => [document.querySelectorAll('a.classical.active'), document.querySelectorAll('a.classical:not(.active)')]
+
+export const getBonannoPisano = () => [document.getElementById('BonannoPisano'), document.querySelectorAll('a:not(#BonannoPisano)')]
